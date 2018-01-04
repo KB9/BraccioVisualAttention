7adbf739d4042ae04ec3cb7af1c0ffa29e4fbc5b
Kavan Bickerstaff <kavanbickerstaff@googlemail.com>
2018-01-04 22:02:57 +0000

    Fixed erroneous setup.py in braccio_gaze_control pkg

--- braccio_gaze_control/setup.py
+++ braccio_gaze_control/setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['braccio_gaze_control'],
+    packages=['.'],
     package_dir={'': '.'},
     requires=['std_msgs', 'rospy']
 )
