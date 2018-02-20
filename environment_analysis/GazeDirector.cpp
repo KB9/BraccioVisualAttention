def463bdc5ea2f99de7b2cfe5d5cddf3c63333f2
Kavan Bickerstaff <kavanbickerstaff@googlemail.com>
2018-02-20 17:21:23 +0000

    Added wrapper for gaze focus components

---
+++ environment_analysis/GazeDirector.cpp
@@ -0,0 +1,66 @@
+#include "GazeDirector.hpp"
+
+// TESTING
+#include <thread>
+#include <chrono>
+
+SceneAnalyzer::ScenePoint toBraccioKinematicsAxes(const SceneAnalyzer::ScenePoint &point)
+{
+	return {point.z * 100.0f, -point.x * 100.0f, point.y * 100.0f};
+}
+
+GazeDirector::GazeDirector(const ros::ServiceClient &obj_detect_client,
+                           std::function<SceneAnalyzer::ScenePoint(const SceneAnalyzer::ScenePoint &camera_point)> camera_to_world,
+                           float diag_fov) :
+	spherical_mapper(diag_fov),
+	scene_analyzer(obj_detect_client, camera_to_world, diag_fov),
+	analyze_scene(false)
+{
+
+}
+
+GazeDirector::GazePoint GazeDirector::next(const SceneAnalyzer::SceneData &data)
+{
+	// Analyze the current scene if the Braccio has been set to look at a new
+	// scene
+	if (analyze_scene)
+	{
+		// Sleep for 1 second to allow the Braccio to stabilize
+		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+		
+		scene_analyzer.analyze(data);
+		analyze_scene = false;
+	}
+
+	// If there are still points in the scene that need to be attended to, do
+	// those first
+	if (scene_analyzer.hasNext())
+	{
+		SceneAnalyzer::ScenePoint point = scene_analyzer.next();
+		return {point.x, point.y, point.z};
+	}
+	// If there are no more points in the scene to be attended to, look at a new
+	// scene and find interesting points within it
+	else if (spherical_mapper.hasNext())
+	{
+		SphericalMapper::GazePoint gaze_point = spherical_mapper.next();
+		auto braccio_point = toBraccioKinematicsAxes({gaze_point.x, gaze_point.y, gaze_point.z});
+		analyze_scene = true;
+		return {braccio_point.x, braccio_point.y, braccio_point.z};
+	}
+	else
+	{
+		// TODO: Do something better than this
+		return {0, 0, 0};
+	}
+}
+
+bool GazeDirector::hasNext()
+{
+	return spherical_mapper.hasNext() || scene_analyzer.hasNext();
+}
+
+void GazeDirector::visualize(const SceneAnalyzer::SceneData &data)
+{
+	scene_analyzer.visualize(data);
+}
---
+++ environment_analysis/GazeDirector.hpp
@@ -0,0 +1,36 @@
+#ifndef _GAZE_DIRECTOR_H_
+#define _GAZE_DIRECTOR_H_
+
+// Infra
+#include <utility>
+
+// ROS
+#include "ros/ros.h"
+
+#include "SphericalMapper.hpp"
+#include "SceneAnalyzer.hpp"
+
+class GazeDirector
+{
+public:
+	struct GazePoint
+	{
+		float x, y, z;
+	};
+
+	GazeDirector(const ros::ServiceClient &obj_detect_client,
+	             std::function<SceneAnalyzer::ScenePoint(const SceneAnalyzer::ScenePoint &camera_point)> camera_to_world,
+	             float diag_fov);
+
+	GazePoint next(const SceneAnalyzer::SceneData &data);
+	bool hasNext();
+
+	void visualize(const SceneAnalyzer::SceneData &data);
+
+private:
+	SphericalMapper spherical_mapper;
+	SceneAnalyzer scene_analyzer;
+	bool analyze_scene;
+};
+
+#endif // _GAZE_DIRECTOR_H_
