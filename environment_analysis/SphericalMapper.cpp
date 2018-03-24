02ed3cd49cc3f388e7240c12c64913390e9a5295
Kavan Bickerstaff <kavanbickerstaff@googlemail.com>
2018-03-24 22:03:16 +0000

    Spherical mapper now uses FOV width/height

--- environment_analysis/SphericalMapper.cpp
+++ environment_analysis/SphericalMapper.cpp
@@ -58,7 +58,8 @@ std::queue<SphericalMapper::GazePoint> SphericalMapper::createGazeSphere(float f
 	// results between -1 and 1, which will be used to define the normalised
 	// distance along the radius (-1 is the bottom of the sphere and 1 is the
 	// top).
-	for (float h = 0.000001f; h < quarter_circle; h += vert_step)
+	float vert_bound = quarter_circle - (vert_step / 2.0f);
+	for (float h = 0.000001f; h < vert_bound; h += vert_step)
 	{
 		// The y-coordinate determines how far the normalised distance is from
 		// the sphere's center, hence it can be used to determine the chord
@@ -66,7 +67,8 @@ std::queue<SphericalMapper::GazePoint> SphericalMapper::createGazeSphere(float f
 		float y = sinf(h);
 		float slice_radius = chordLength(y) * 0.5f;
 		ROS_INFO("===== Slice at %.3f from the center (0.0f) (RADIUS: %.3f) =====", y, slice_radius);
-		for (float w = 0.0f; w < full_circle; w += horiz_step)
+		float horiz_bound = full_circle - (horiz_step / 2.0f);
+		for (float w = 0.0f; w < horiz_bound; w += horiz_step)
 		{
 			// This is a RHS-with-Y-up coordinate system, so w will change x/z
 			// whilst h will change the y coordinate
